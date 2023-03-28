9d32c2b1a8286abca5f8a845f79065e6321b68ab
pranavppt <pranavptrivedi14@gmail.com>
2023-03-28 17:55:21 +0530

    resNav4

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function onClick(){
     document.getElementById("nav__menu").classList.toggle("list");
+    document.getElementById("menuIcon").classList.toggle("list");
+    document.getElementById("close").classList.toggle("list");
 }
