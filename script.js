81756030a1fe98fe57a6934ecc6933f96f47b53b
pranavppt <pranavptrivedi14@gmail.com>
2023-03-29 22:51:41 +0530

    signup/signin

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ function onClick(){
     document.getElementById("menuIcon").classList.toggle("list");
     document.getElementById("close").classList.toggle("list");
 }
+function signupToggle(){
+    var container = document.querySelector('.header');
+    container.classList.toggle('active');
+    var popUp = document.querySelector('.signup-form');
+    popUp.classList.toggle('active')
+}
+function loginToggle(){
+    var container = document.querySelector('.header');
+    container.classList.toggle('active');
+    var popUp = document.querySelector('.login-form');
+    popUp.classList.toggle('active')
+}
